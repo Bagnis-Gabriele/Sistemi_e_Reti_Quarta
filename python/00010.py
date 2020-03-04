c5a445d41e1d121ded3136248d280bdb104265ba
Gabriele Bagnis <gabriele.bagnis@itiscuneo.eu>
2020-03-04 15:38:24 +0100

    crea matrice modo 1

--- python/00010.py
+++ python/00010.py
@@ -1,5 +1,5 @@
 """
-Dati in input i nodi e i link dei grafo, disegnarlo
+Creare la matrice di un grafico orientato
 """
 link = []
 nodi = int(input("quanti nodi ci sono?")) + 1
@@ -7,14 +7,16 @@ nodi = int(input("quanti nodi ci sono?")) + 1
 #insert data
 for k in range (1,nodi):
     node = []
-    for i in range (1,nodi):
+    for i in range (0,nodi):
         data=2
-        while data!=1 and data !=0:
-            data= int(input(f"il nodo {k} è collegato al nodo {i}? (1 si, 0 no)"))
+        if k==1:
+            data=1
+        else:
+            while data!=1 and data !=0:
+                data= int(input(f"il nodo {k} è collegato al nodo {i}? (1 si, 0 no)"))
         node.append(data)
     link.append(node[:])
     
 # drow matrix
 for element in link:
     print(element)
-        
